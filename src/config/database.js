ded48d00ff6c7fa60f0f4d1fd541a21e90cc1363
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-03-11 21:29:03 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,23 @@
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
+  production: {
+    dialect: process.env.DB_DIALECT,
+    use_env_variable: "DATABASE_URL",
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  }
 };
